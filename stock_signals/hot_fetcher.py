cf1ed531b33641107288dc9f4ae4c5a67b279fb9
sailor <sailor@localhost>
2026-09-05 00:18:09 +0800

    feat: replace Sina volume ranking with Futu hot list + top movers API
    - Add fetch_us_hot_stocks_futu() using get_top_movers_rank with market cap filter
    - Add fetch_us_hot_stocks_futu_hotlist() using get_hot_list by trade heat
    - Merge hot list + top movers for comprehensive coverage
    - Fallback to Sina volume ranking and static pool on Futu failure
    - Restored full 335-stock pool (MKT_CAP + _US_STATIC_POOL)

--- stock_signals/hot_fetcher.py
+++ stock_signals/hot_fetcher.py
@@ -421,6 +421,470 @@ def _fetch_us_quotes_from_sina(ticker_list, batch_size=80):
 
 
 def fetch_us_hot_stocks(top_n=300):
+    """Fetch hot US stocks - Futu API first, Sina fallback, static pool last."""
+    pool = list(dict.fromkeys(_US_STATIC_POOL))
+    pool = [t for t in pool if t in MKT_CAP and MKT_CAP[t] >= MIN_MARKET_CAP]
+    logger.info('  US static pool: ' + str(len(pool)) + ' stocks (>=10B market cap)')
+
+    # Method 1: Futu top movers + hot list (merged, by heat + price action)
+    futu_codes = fetch_us_hot_stocks_futu(top_n)
+    if futu_codes:
+        try:
+            hotlist_codes = fetch_us_hot_stocks_futu_hotlist(50)
+            if hotlist_codes:
+                seen = set()
+                merged = []
+                for c in hotlist_codes + futu_codes:
+                    if c not in seen:
+                        seen.add(c)
+                        merged.append(c)
+                logger.info('  US hot merged: ' + str(len(merged)) + ' stocks')
+                return merged[:top_n]
+        except Exception:
+            pass
+        return futu_codes[:top_n]
+
+    # Method 2: Sina volume-based fallback
+    try:
+        t = time.time()
+        quotes = _fetch_us_quotes_from_sina(pool)
+        if quotes:
+            quotes.sort(key=lambda x: x['volume'], reverse=True)
+            codes = ['US.' + s['ticker'] for s in quotes[:top_n]]
+            elapsed = time.time() - t
+            logger.info('  US hot (Sina volume): ' + str(len(codes)) + ' stocks (' + str(round(elapsed, 1)) + 's)')
+            return codes
+        else:
+            logger.warning('  Sina no data')
+    except Exception as e:
+        logger.warning('  US Sina hot fetch failed: ' + str(e))
+
+    # Method 3: Static pool
+    logger.info('  US: using static pool')
+    return ['US.' + t for t in pool[:top_n]]
+# -*- coding: utf-8 -*-
+
+import logging, time, os, urllib.request
+from typing import List
+logger = logging.getLogger("tech-signal-FUTU-skill")
+for _k in list(os.environ.keys()):
+    if "proxy" in _k.lower(): os.environ.pop(_k, None)
+os.environ.setdefault("no_proxy", "*")
+os.environ.setdefault("NO_PROXY", "*")
+MIN_MARKET_CAP = 10e9
+MKT_CAP = {
+    'NVDA': 2800000000000.0,
+    'AAPL': 2900000000000.0,
+    'MSFT': 3100000000000.0,
+    'GOOG': 2000000000000.0,
+    'GOOGL': 2000000000000.0,
+    'AMZN': 1800000000000.0,
+    'META': 1200000000000.0,
+    'TSLA': 500000000000.0,
+    'AVGO': 600000000000.0,
+    'CSCO': 200000000000.0,
+    'ORCL': 350000000000.0,
+    'AMD': 200000000000.0,
+    'INTC': 100000000000.0,
+    'QCOM': 180000000000.0,
+    'MU': 100000000000.0,
+    'NXPI': 60000000000.0,
+    'AMAT': 150000000000.0,
+    'LRCX': 80000000000.0,
+    'ASML': 300000000000.0,
+    'TXN': 170000000000.0,
+    'NOW': 200000000000.0,
+    'CRM': 250000000000.0,
+    'ADBE': 220000000000.0,
+    'PANW': 100000000000.0,
+    'PLTR': 50000000000.0,
+    'LLY': 600000000000.0,
+    'ABBV': 280000000000.0,
+    'JNJ': 380000000000.0,
+    'MRK': 300000000000.0,
+    'PFE': 150000000000.0,
+    'BMY': 80000000000.0,
+    'UNH': 480000000000.0,
+    'TMO': 200000000000.0,
+    'DHR': 150000000000.0,
+    'ABT': 180000000000.0,
+    'V': 500000000000.0,
+    'MA': 400000000000.0,
+    'BRK-B': 800000000000.0,
+    'WMT': 450000000000.0,
+    'COST': 300000000000.0,
+    'HD': 350000000000.0,
+    'MCD': 180000000000.0,
+    'NKE': 100000000000.0,
+    'DIS': 180000000000.0,
+    'NFLX': 200000000000.0,
+    'XOM': 400000000000.0,
+    'COP': 120000000000.0,
+    'OXY': 60000000000.0,
+    'HON': 150000000000.0,
+    'CAT': 150000000000.0,
+    'BA': 100000000100.0,
+    'GE': 150000000000.0,
+    'UNP': 130000000000.0,
+    'UPS': 130000000000.0,
+    'LMT': 100000000000.0,
+    'RTX': 120000000000.0,
+    'FCX': 50000000000.0,
+    'NEM': 40000000000.0,
+    'EMR': 50000000000.0,
+    'DASH': 80000000000.0,
+    'UBER': 120000000000.0,
+    'ABNB': 80000000000.0,
+    'ZTS': 80000000000.0,
+    'LOW': 120000000000.0,
+    'TJX': 100000000000.0,
+    'BKNG': 70000000000.0,
+    'LULU': 50000000000.0,
+    'SBUX': 100000000000.0,
+    'CMCSA': 150000000000.0,
+    'MDLZ': 50000000000.0,
+    'HSY': 50000000000.0,
+    'CL': 50000000000.0,
+    'UL': 80000000000.0,
+    'GIS': 30000000000.0,
+    'KHC': 20000000000.0,
+    'K': 10000000000.0,
+    'MO': 60000000000.0,
+    'PM': 120000000000.0,
+    'BTI': 20000000000.0,
+    'CPB': 20000000000.0,
+    'CAG': 10000000000.0,
+    'PEP': 220000000000.0,
+    'KO': 250000000000.0,
+    'CLX': 50000000000.0,
+    'KMB': 50000000000.0,
+    'BG': 20000000000.0,
+    'TSN': 30000000000.0,
+    'HPQ': 20000000000.0,
+    'ADM': 30000000000.0,
+    'CMG': 30000000000.0,
+    'HST': 10000000000.0,
+    'YUM': 40000000000.0,
+    'EQT': 10000000000.0,
+    'SLB': 50000000000.0,
+    'HAL': 20000000000.0,
+    'MPC': 30000000000.0,
+    'PSX': 60000000000.0,
+    'VLO': 30000000000.0,
+    'OKE': 30000000000.0,
+    'WMB': 30000000000.0,
+    'ET': 50000000000.0,
+    'LNG': 30000000000.0,
+    'MPLX': 40000000000.0,
+    'KMI': 40000000000.0,
+    'MRNA': 10000000000.0,
+    'ALGN': 20000000000.0,
+    'MOH': 30000000000.0,
+    'UHS': 10000000000.0,
+    'PINS': 20000000000.0,
+    'F': 40000000000.0,
+    'GM': 50000000000.0,
+    'MAR': 30000000000.0,
+    'HLT': 40000000000.0,
+    'APLE': 10000000000.0,
+    'TGT': 50000000000.0,
+    'PARA': 10000000000.0,
+    'WBD': 20000000000.0,
+    'QSR': 30000000000.0,
+    'SPGI': 130000000000.0,
+    'PGR': 60000000000.0,
+    'CB': 80000000000.0,
+    'TRV': 30000000000.0,
+    'ALL': 50000000000.0,
+    'AON': 30000000000.0,
+    'AJG': 20000000000.0,
+    'WTW': 40000000000.0,
+    'BLK': 120000000000.0,
+    'SCHW': 100000000000.0,
+    'CME': 80000000000.0,
+    'ICE': 80000000000.0,
+    'MCO': 30000000000.0,
+    'AMGN': 150000000000.0,
+    'GILD': 120000000000.0,
+    'REGN': 100000000000.0,
+    'BIIB': 40000000000.0,
+    'VRTX': 100000000000.0,
+    'JPM': 500000000000.0,
+    'BAC': 250000000000.0,
+    'WFC': 150000000000.0,
+    'C': 100000000000.0,
+    'GS': 120000000000.0,
+    'MS': 100000000000.0,
+    'USB': 60000000000.0,
+    'PNC': 50000000000.0,
+    'TFC': 40000000000.0,
+    'BK': 30000000000.0,
+    'AXP': 150000000000.0,
+    'MMC': 60000000000.0,
+    'AIG': 40000000000.0,
+    'MET': 40000000000.0,
+    'PRU': 50000000000.0,
+    'AFL': 20000000000.0,
+    'HIG': 30000000000.0,
+    'CINF': 20000000000.0,
+    'L': 20000000000.0,
+    'RHI': 10000000000.0,
+    'J': 20000000000.0,
+    'HII': 30000000000.0,
+    'KTOS': 10000000000.0,
+    'EW': 20000000000.0,
+    'HOLX': 20000000000.0,
+    'SYK': 50000000000.0,
+    'BDX': 40000000000.0,
+    'BSX': 80000000000.0,
+    'MDT': 50000000000.0,
+    'ABC': 40000000000.0,
+    'CI': 150000000000.0,
+    'HUM': 50000000000.0,
+    'CNC': 40000000000.0,
+    'ANTM': 50000000000.0,
+    'ELV': 50000000000.0,
+    'WBA': 10000000000.0,
+    'CVS': 80000000000.0,
+    'GSK': 70000000000.0,
+    'AZN': 180000000000.0,
+    'NVO': 350000000000.0,
+    'SNY': 80000000000.0,
+    'SNPS': 130000000000.0,
+    'CDNS': 80000000000.0,
+    'ADSK': 70000000000.0,
+    'ANSS': 30000000000.0,
+    'FTNT': 50000000000.0,
+    'NET': 100000000000.0,
+    'SNOW': 50000000000.0,
+    'CRWD': 70000000000.0,
+    'DDOG': 40000000000.0,
+    'MDB': 50000000000.0,
+    'SHOP': 100000000000.0,
+    'SQ': 40000000000.0,
+    'PYPL': 70000000000.0,
+    'IBM': 170000000000.0,
+    'DELL': 70000000000.0,
+    'HPE': 30000000000.0,
+    'ACN': 220000000000.0,
+    'ZBH': 50000000000.0,
+    'COO': 40000000000.0,
+    'ISRG': 130000000000.0,
+    'BR': 15000000000.0,
+    'FIS': 40000000000.0,
+    'ADP': 100000000000.0,
+    'DG': 30000000000.0,
+    'DLTR': 20000000000.0,
+    'GPS': 10000000000.0,
+    'ANF': 10000000000.0,
+    'RL': 20000000000.0,
+    'TPR': 20000000000.0,
+    'M': 10000000000.0,
+    'KSS': 10000000000.0,
+    'DPZ': 30000000000.0,
+    'SWK': 10000000000.0,
+    'TM': 250000000000.0,
+    'APTV': 25000000000.0,
+    'BWA': 20000000000.0,
+    'AZO': 30000000000.0,
+    'LEA': 20000000000.0,
+    'ALK': 15000000000.0,
+    'NCLH': 15000000000.0,
+    'WYNN': 20000000000.0,
+    'MGM': 25000000000.0,
+    'DKNG': 20000000000.0,
+    'LYV': 10000000000.0,
+    'XEL': 20000000000.0,
+    'ED': 20000000000.0,
+    'WEC': 20000000000.0,
+    'ES': 20000000000.0,
+    'D': 30000000000.0,
+    'DUK': 50000000000.0,
+    'SO': 50000000000.0,
+    'NEE': 140000000000.0,
+    'AEP': 30000000000.0,
+    'EXC': 20000000000.0,
+    'SRE': 40000000000.0,
+    'PEG': 50000000000.0,
+    'ATO': 20000000000.0,
+    'EIX': 30000000000.0,
+    'PPL': 20000000000.0,
+    'FE': 20000000000.0,
+    'ETR': 40000000000.0,
+    'PNW': 10000000000.0,
+    'AEE': 10000000000.0,
+    'LNT': 10000000000.0,
+    'CMS': 10000000000.0,
+    'NI': 10000000000.0,
+    'AMT': 80000000000.0,
+    'EQIX': 80000000000.0,
+    'BXP': 30000000000.0,
+    'PSA': 60000000000.0,
+    'WELL': 30000000000.0,
+    'DLR': 50000000000.0,
+    'O': 50000000000.0,
+    'SPG': 30000000000.0,
+    'ARE': 20000000000.0,
+    'MAA': 20000000000.0,
+    'KIM': 20000000000.0,
+    'REG': 20000000000.0,
+    'FRT': 10000000000.0,
+    'VTR': 20000000000.0,
+    'ESS': 10000000000.0,
+    'EXR': 20000000000.0,
+    'AVB': 30000000000.0,
+    'EQR': 30000000000.0,
+    'SBAC': 30000000000.0,
+    'CCI': 50000000000.0,
+    'PLD': 50000000000.0,
+    'CUZ': 20000000000.0,
+    'DEI': 10000000000.0,
+    'SUI': 10000000000.0,
+    'HIW': 10000000000.0,
+    'WPC': 20000000000.0,
+    'AA': 10000000000.0,
+    'NUE': 40000000000.0,
+    'STLD': 20000000000.0,
+    'ECL': 50000000000.0,
+    'DD': 60000000000.0,
+    'DOW': 40000000000.0,
+    'EMN': 10000000000.0,
+    'CTVA': 40000000000.0,
+    'MMM': 60000000000.0,
+    'MOS': 30000000000.0,
+    'GOLD': 30000000000.0,
+    'WPM': 40000000000.0,
+    'FNV': 50000000000.0,
+    'SCCO': 30000000000.0,
+    'CLF': 10000000000.0,
+    'CE': 20000000000.0,
+    'MLM': 30000000000.0,
+    'VMC': 30000000000.0,
+    'X': 30000000000.0,
+    'RS': 10000000000.0,
+    'PKG': 30000000000.0,
+    'IP': 20000000000.0,
+    'CC': 10000000000.0,
+    'FMC': 20000000000.0,
+    'APD': 100000000000.0,
+    'LIN': 200000000000.0,
+    'SHW': 60000000000.0,
+    'CVX': 300000000000.0,
+    'EOG': 70000000000.0,
+    'PXD': 60000000000.0,
+    'FANG': 50000000000.0,
+    'HES': 30000000000.0,
+    'DVN': 30000000000.0,
+    'MRO': 10000000000.0,
+    'APA': 10000000000.0,
+    'CNQ': 60000000000.0,
+    'SU': 40000000000.0,
+    'WES': 30000000000.0,
+    'VZ': 180000000000.0,
+    'T': 130000000000.0,
+    'CHTR': 100000000000.0,
+    'TMUS': 200000000000.0,
+    'LUMN': 30000000000.0,
+    'FYBR': 30000000000.0,
+    'CMCSA': 150000000000.0,
+    'OMC': 30000000000.0,
+    'IPG': 20000000000.0,
+    'NWSA': 20000000000.0,
+    'FOXA': 20000000000.0,
+    'FISV': 50000000000.0,
+    'GPN': 30000000000.0,
+    'TSCO': 30000000000.0,
+    'ROST': 30000000000.0,
+    'BBY': 30000000000.0,
+    'AAP': 20000000000.0,
+    'TWLO': 30000000000.0,
+    'APH': 60000000000.0,
+    'TEL': 30000000000.0,
+    'GLW': 50000000000.0,
+    'KEYS': 30000000000.0,
+    'ZBRA': 20000000000.0,
+    'JNPR': 20000000000.0,
+    'NTAP': 20000000000.0,
+    'STX': 20000000000.0,
+    'WDC': 20000000000.0,
+    'FDX': 70000000000.0,
+    'WBA': 10000000000.0,
+    'SAM': 30000000000.0
+}
+_US_STATIC_POOL = [
+    'AAPL', 'MSFT', 'GOOG', 'GOOGL', 'AMZN', 'META', 'NVDA', 'TSLA', 'AVGO', 'CSCO',
+    'ORCL', 'AMD', 'INTC', 'QCOM', 'MU', 'NXPI', 'AMAT', 'LRCX', 'ASML', 'TXN',
+    'NOW', 'CRM', 'ADBE', 'PANW', 'PLTR', 'LLY', 'ABBV', 'JNJ', 'MRK', 'PFE',
+    'BMY', 'UNH', 'TMO', 'DHR', 'ABT', 'AMGN', 'GILD', 'REGN', 'BIIB', 'VRTX',
+    'V', 'MA', 'BRK-B', 'WMT', 'COST', 'HD', 'MCD', 'NKE', 'DIS', 'NFLX',
+    'XOM', 'COP', 'OXY', 'HON', 'CAT', 'BA', 'GE', 'UNP', 'UPS', 'LMT',
+    'RTX', 'FCX', 'NEM', 'EMR', 'DASH', 'UBER', 'ABNB', 'ZTS', 'LOW', 'TJX',
+    'BKNG', 'LULU', 'SBUX', 'CMCSA', 'MDLZ', 'HSY', 'CL', 'UL', 'GIS', 'KHC',
+    'K', 'MO', 'PM', 'BTI', 'CPB', 'CAG', 'PEP', 'KO', 'CLX', 'KMB',
+    'BG', 'TSN', 'HPQ', 'ADM', 'CMG', 'HST', 'YUM', 'EQT', 'SLB', 'HAL',
+    'MPC', 'PSX', 'VLO', 'OKE', 'WMB', 'ET', 'LNG', 'MPLX', 'KMI', 'MRNA',
+    'ALGN', 'MOH', 'UHS', 'PINS', 'F', 'GM', 'MAR', 'HLT', 'APLE', 'TGT',
+    'PARA', 'WBD', 'QSR', 'SPGI', 'PGR', 'CB', 'TRV', 'ALL', 'AON', 'AJG',
+    'WTW', 'BLK', 'SCHW', 'CME', 'ICE', 'MCO', 'JPM', 'BAC', 'WFC', 'C',
+    'GS', 'MS', 'USB', 'PNC', 'TFC', 'BK', 'AXP', 'MMC', 'AIG', 'MET',
+    'PRU', 'AFL', 'HIG', 'CINF', 'L', 'RHI', 'J', 'HII', 'KTOS', 'EW',
+    'HOLX', 'SYK', 'BDX', 'BSX', 'MDT', 'ABC', 'CI', 'HUM', 'CNC', 'ANTM',
+    'ELV', 'CVS', 'GSK', 'AZN', 'NVO', 'SNY', 'SNPS', 'CDNS', 'ADSK', 'ANSS',
+    'FTNT', 'NET', 'SNOW', 'CRWD', 'DDOG', 'MDB', 'SHOP', 'SQ', 'PYPL', 'IBM',
+    'DELL', 'HPE', 'ACN', 'ZBH', 'COO', 'ISRG', 'BR', 'FIS', 'ADP', 'DG',
+    'DLTR', 'GPS', 'ANF', 'RL', 'TPR', 'M', 'KSS', 'DPZ', 'SWK', 'TM',
+    'APTV', 'BWA', 'AZO', 'LEA', 'ALK', 'NCLH', 'WYNN', 'MGM', 'DKNG', 'LYV',
+    'XEL', 'ED', 'WEC', 'ES', 'D', 'DUK', 'SO', 'NEE', 'AEP', 'EXC',
+    'SRE', 'PEG', 'ATO', 'EIX', 'PPL', 'FE', 'ETR', 'PNW', 'AEE', 'LNT',
+    'CMS', 'NI', 'AMT', 'EQIX', 'BXP', 'PSA', 'WELL', 'DLR', 'O', 'SPG',
+    'ARE', 'MAA', 'KIM', 'REG', 'FRT', 'VTR', 'ESS', 'EXR', 'AVB', 'EQR',
+    'SBAC', 'CCI', 'PLD', 'CUZ', 'DEI', 'SUI', 'HIW', 'WPC', 'AA', 'NUE',
+    'STLD', 'ECL', 'DD', 'DOW', 'EMN', 'CTVA', 'MMM', 'MOS', 'GOLD', 'WPM',
+    'FNV', 'SCCO', 'CLF', 'CE', 'MLM', 'VMC', 'X', 'RS', 'PKG', 'IP',
+    'CC', 'FMC', 'APD', 'LIN', 'SHW', 'CVX', 'EOG', 'PXD', 'FANG', 'HES',
+    'DVN', 'MRO', 'APA', 'CNQ', 'SU', 'WES', 'VZ', 'T', 'CHTR', 'TMUS',
+    'LUMN', 'FYBR', 'OMC', 'IPG', 'NWSA', 'FOXA', 'FISV', 'GPN', 'TSCO', 'ROST',
+    'BBY', 'AAP', 'TWLO', 'APH', 'TEL', 'GLW', 'KEYS', 'ZBRA', 'JNPR', 'NTAP',
+    'STX', 'WDC', 'FDX', 'WBA', 'SAM',
+]
+
+def _fetch_us_quotes_from_sina(ticker_list, batch_size=80):
+    all_stocks = []
+    for i in range(0, len(ticker_list), batch_size):
+        batch = ticker_list[i:i+batch_size]
+        gbs = ['gb_' + t.lower() for t in batch]
+        tickers = ','.join(gbs)
+        url = 'http://hq.sinajs.cn/list=' + tickers
+        try:
+            req = urllib.request.Request(url, headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+                'Referer': 'http://finance.sina.com.cn/'
+            })
+            resp = urllib.request.urlopen(req, timeout=10)
+            data = resp.read().decode('gbk', errors='replace')
+            for line in data.strip().split(chr(10)):
+                if 'hq_str_gb_' not in line:
+                    continue
+                parts = line.split('=')
+                if len(parts) < 2:
+                    continue
+                ticker = parts[0].replace('var hq_str_gb_', '').strip().upper()
+                vals = parts[1].split(',')
+                if len(vals) < 11:
+                    continue
+                try:
+                    price = float(vals[1])
+                    vol = int(float(vals[10])) if vals[10] else 0
+                    if price > 0 and vol > 0:
+                        all_stocks.append({'ticker': ticker, 'price': price, 'volume': vol})
+                except Exception:
+                    continue
+        except Exception as e:
+            logger.warning('Sina batch failed: ' + str(e))
+    return all_stocks
+
+
+def fetch_us_hot_stocks(top_n=300):
     pool = list(dict.fromkeys(_US_STATIC_POOL))
     pool = [t for t in pool if t in MKT_CAP and MKT_CAP[t] >= MIN_MARKET_CAP]
     logger.info('  US static pool: ' + str(len(pool)) + ' stocks (>=10B market cap)')
